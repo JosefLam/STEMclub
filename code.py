2035d4bb231902917072c9312bff4ef8abe0c7e7
JosefLam <113189082+JosefLam@users.noreply.github.com>
2023-05-17 15:32:10 +0100

    Update code.py

--- code.py
+++ code.py
@@ -43,6 +43,7 @@ CRASH1 = 49
 CRASH2 = 57
 
 
+
 buttonA = digitalio.DigitalInOut(board.GP12)
 buttonA.switch_to_input(pull=digitalio.Pull.UP)
 buttonB = digitalio.DigitalInOut(board.GP13)
@@ -51,6 +52,8 @@ buttonX = digitalio.DigitalInOut(board.GP14)
 buttonX.switch_to_input(pull=digitalio.Pull.UP)
 buttonY = digitalio.DigitalInOut(board.GP15)
 buttonY.switch_to_input(pull=digitalio.Pull.UP)
+
+btnCd = 0.2
 
 # Release any resources currently in use for the displays
 displayio.release_displays()
@@ -108,7 +111,7 @@ text_group1 = displayio.Group(
 )
 text_group1.append(text_area1)  # Subgroup for text scaling
 
-midi_msg_area = label.Label(terminalio.FONT, text="Last MIDI msg:", color=TEXT_COLOR)
+midi_msg_area = label.Label(terminalio.FONT, text="", color=TEXT_COLOR)
 midi_msg_width = midi_msg_area.bounding_box[2] * FONTSCALE
 midi_msg_group = displayio.Group(
     scale=FONTSCALE,
@@ -117,7 +120,7 @@ midi_msg_group = displayio.Group(
 )
 midi_msg_group.append(midi_msg_area)
 
-midi_details_area = label.Label(terminalio.FONT, text="CH: N: V:", color=TEXT_COLOR)
+midi_details_area = label.Label(terminalio.FONT, text="", color=TEXT_COLOR)
 midi_details_width = midi_msg_area.bounding_box[2] * FONTSCALE
 midi_details_group = displayio.Group(
     scale=FONTSCALE,
@@ -146,65 +149,119 @@ midi = adafruit_midi.MIDI(
     debug=False,
 )
 
-while True:
-    msg = midi.receive()
-    if msg != None:
-        midi_msg_area.text=type(msg).__name__
-        try:
-            midi_details_area.text="Cha:{} Not:{} Vel:{}".format(msg.channel, msg.note, msg.velocity)
-        except:
-            midi_details_area.text = ""
-        
-    
+
+
+
+
+
+
+
+
+
+
+
+noteString = ""
+setNoteNum = 1
+while not (setNoteNum == 9):
+    text_area.text = "HiHatClosed"
+    text_area1.text = str(setNoteNum)+"/8"
     if not buttonA.value:
-        text_area1.text = "snare"
-        midi.send(NoteOn(SNARE, 120))  # G sharp 2nd octave
-        while not buttonA.value:
-            time.sleep(0.1)
-        midi.send(NoteOff("C2"))
-        text_area1.text = ""
+        noteString = (noteString)+"0"
+        setNoteNum = setNoteNum + 1
+        time.sleep(btnCd)
+    elif not buttonX.value:
+        noteString = (noteString)+"1"
+        setNoteNum = setNoteNum + 1
+        time.sleep(btnCd)
         
-    if not buttonB.value:
-        text_area1.text = "closed hihat"
-        midi.send(NoteOn(CLOSEDHIHAT, 120))  # G sharp 2nd octave
-        while not buttonB.value:
-            time.sleep(0.1)
-        midi.send(NoteOff("E2"))
-        text_area1.text = ""
-        
-    if not buttonX.value:
-        text_area1.text = "kick"
-        midi.send(NoteOn(ELECTRICKICK, 120))  # G sharp 2nd octave
-        while not buttonX.value:
-            time.sleep(0.1)
-        midi.send(NoteOff("G2"))
-        text_area1.text = ""
-        
-    if not buttonY.value:
-        text_area1.text = "open hihat"
-        midi.send(NoteOn(OPENHIHAT, 120))  # G sharp 2nd octave
-        while not buttonY.value:
-            time.sleep(0.1)
-        midi.send(NoteOff("C3"))
-        text_area1.text = ""
+notePage = open("notes.txt", "w")
+notePage.writeline(noteString)
+notePage.Close()
+
+
+
+
+
+
+
+
+
+
+
+
+notePage = open("notes.txt", "r") 
+    
+hiHatCNotes = notePage.readline().split("\t")[0]
+hiHatONotes = notePage.readline().split("\t")[0]
+kickNotes = notePage.readline().split("\t")[0]
+snareNotes = notePage.readline().split("\t")[0]
+crashNotes = notePage.readline().split("\t")[0]
+    
+notePage.close()
+
+tempo = 60
+
   
-    notePage = open("notes.txt", "r") 
-    
-    hiHatCNotes = notePage.readline().split("\t")[0]
-    hiHatONotes = notePage.readline().split("\t")[0]
-    kickNotes = notePage.readline().split("\t")[0]
-    snareNotes = notePage.readline().split("\t")[0]
-    
-    notePage.close()
-    
-    noteNum = 0
-    
-    while noteNum != 5:
-        if int(hiHatCNotes[noteNum]) == 1:
-            text_area1.text = "closed hihat"
-            midi.send(NoteOn(CLOSEDHIHAT, 120))
+start = 0
+
+play = False
+
+while True:
+    text_area1.text = str(tempo)+"BPM"
+    while not buttonA.value:
+        if tempo > 50:
+            tempo = tempo - 5
+            text_area1.text = str(tempo)+"BPM"
+            time.sleep(btnCd)
+    while not buttonX.value:
+        if tempo < 360:
+            tempo = tempo + 5
+            text_area1.text = str(tempo)+"BPM"
+            time.sleep(btnCd)
+    if not buttonY.value:
+        text_area1.text = "playing"
+        play = True
+        delay = 1/(tempo/60)
+        time.sleep(btnCd)
+        while (play == True):
+            noteNum = 0
+            
+            while noteNum != 8:
+            
+                if int(hiHatCNotes[noteNum]) == 1:
+                    midi.send(NoteOn(CLOSEDHIHAT, 120))
+                
+                if int(hiHatONotes[noteNum]) == 1:
+                    midi.send(NoteOn(OPENHIHAT, 120))
+                
+                if int(kickNotes[noteNum]) == 1:
+                    midi.send(NoteOn(ELECTRICKICK, 120))
+                
+                if int(snareNotes[noteNum]) == 1:
+                    midi.send(NoteOn(SNARE, 120))
+                
+                if int(crashNotes[noteNum]) == 1:
+                    midi.send(NoteOn(CRASH1, 120))
+            
+                noteNum = noteNum + 1
+                if not buttonY.value:
+                    play = False
+                    text_area1.text = "stopping"
+                    time.sleep(1)
+                
+                time.sleep(delay)
+
+msg = midi.receive()
+if msg != None:
+    midi_msg_area.text=type(msg).__name__
+    try:
+        midi_details_area.text="Cha:{} Not:{} Vel:{}".format(msg.channel, msg.note, msg.velocity)
+    except:
+        midi_details_area.text = ""
+
     
     
     #time.sleep(0.1)
     #pass
+
 
